e3ce4c680b14c55a1120ad77d8c1d31c76d4a6fe
Vyom Sharma <93402393+vyom1611@users.noreply.github.com>
2022-12-27 15:33:23 -0600

    Add files via upload

---
+++ EurekaTut/src/test/java/com/example/eurekatut/EurekaTutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekatut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaTutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
