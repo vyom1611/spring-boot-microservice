dca75063f69b17cb3600dad05f11db5c036d2a1e
vyom1611 <sharm843@umn.edu>
2022-12-27 14:51:20 -0600

    EurekaServer added and connected to account and user microservice

--- AccountApi-microservice/src/main/java/com/example/accountapimicroservice/AccountApiMicroserviceApplication.java
+++ AccountApi-microservice/src/main/java/com/example/accountapimicroservice/AccountApiMicroserviceApplication.java
@@ -1,5 +1,6 @@
 package com.example.accountapimicroservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,4 @@ public class AccountApiMicroserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AccountApiMicroserviceApplication.class, args);
 	}
-
 }
